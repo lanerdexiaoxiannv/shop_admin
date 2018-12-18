7dd1d3a46b4f154004e92984dc924ccea5ec6e6e
stt <stt@qq.com>
2018-12-18 23:16:38 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- <router-link></router-link> -->
+   <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
